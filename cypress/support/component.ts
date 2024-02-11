ecbb55030012419645956c6d20623b1274b27b38
Avram Dames <avram.dames@gmail.com>
2024-02-11 20:39:57 +0200

    add vue wrapper

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -32,7 +32,11 @@ declare global {
   }
 }
 
-Cypress.Commands.add('mount', mount)
+Cypress.Commands.add('mount', (...args) => {
+  return mount(...args).then(({ wrapper }) => {
+    return cy.wrap(wrapper).as('vue')
+  })
+})
 
 // Example use:
 // cy.mount(MyComponent)
